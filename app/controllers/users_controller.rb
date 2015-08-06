ccddb16dc9963603ab4822572707971f784b3d2e
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-06 17:32:17 -0500

    Instead of taking to users/:id, take to profile_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to(@user, :notice => "Welcome to Wellness Tracker!")
+      redirect_to profile_path, :notice => "Welcome to Wellness Tracker!"
     else
       render :new, :notice => "Unable to create a new user."
     end
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
   
   def update
     if @user.update(user_params)
-      redirect_to @user, :notice => "Account updated!"
+      redirect_to profile_path, :notice => "Account updated!"
     else
       render :edit, :notice => "Unable to update your account."
     end
@@ -38,18 +38,18 @@ class UsersController < ApplicationController
     if @user.destroy
       redirect_to login_path, :notice => "Your account was deleted."
     else
-      redirect_to @user, :notice => "Sorry.  Something went wrong.  We are unable to deleter your account."
+      redirect_to profile_path, :notice => "Sorry.  Something went wrong.  We are unable to deleter your account."
     end
   end
   
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :profile_picture)
   end
   
   def prevent_duplicate_sessions
     if @user
-      redirect_to @user, :notice => "You are already logged in.  To create a new account, log out first."
+      redirect_to profile_path, :notice => "You are already logged in.  To create a new account, log out first."
     end
   end
   
