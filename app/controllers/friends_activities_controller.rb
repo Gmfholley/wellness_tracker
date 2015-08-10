6de186ed5699583f356902ee44f2e538f8b30a22
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-10 18:36:43 -0500

    Adds set friend method to controller as private method

--- app/controllers/friends_activities_controller.rb
+++ app/controllers/friends_activities_controller.rb
@@ -1,7 +1,15 @@
 class FriendsActivitiesController < ApplicationController
+  before_action :set_friend
+  
+  
   def index
   end
 
   def show
+  end
+  
+  private
+  def set_friend
+    @friend = User.find(params["id"])
   end
 end
