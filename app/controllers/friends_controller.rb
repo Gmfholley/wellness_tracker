965dc8def7969210d61ebbdee6ad304ec2881238
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-10 14:38:42 -0500

    Basic friends controller methods

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -4,4 +4,22 @@ class FriendsController < ApplicationController
 
   def create
   end
+  
+  def show
+  end
+  
+  def index
+  end
+  
+  def destroy
+  end
+  
+  def edit
+    redirect_to friends_path, notice: "Oops!  You were directed to the wrong place. If you keep getting this message, contact the web master."
+  end
+  
+  def update
+    redirect_to friends_path, notice: "Oops!  You were directed to the wrong place. If you keep getting this message, contact the web master."
+  end
+  
 end
