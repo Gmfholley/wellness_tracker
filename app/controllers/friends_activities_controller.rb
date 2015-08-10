15ce35ae6f78cc98bf14840472c35a21ab93e74e
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-10 18:42:59 -0500

    Do not need show page for friends activities.  That will take you to the profile page.

--- app/controllers/friends_activities_controller.rb
+++ app/controllers/friends_activities_controller.rb
@@ -1,15 +1,7 @@
 class FriendsActivitiesController < ApplicationController
-  before_action :set_friend
   
   
   def index
   end
-
-  def show
-  end
   
-  private
-  def set_friend
-    @friend = User.find(params["id"])
-  end
 end
