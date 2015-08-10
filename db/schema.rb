774f53c7a20025e07894ed4f0e2e3ab296030b25
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-10 14:02:37 -0500

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810185216) do
+ActiveRecord::Schema.define(version: 20150810185854) do
 
   create_table "cheers", force: :cascade do |t|
     t.integer  "exercise_event_id"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20150810185216) do
 
   create_table "exercise_units", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friends", id: false, force: :cascade do |t|
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
