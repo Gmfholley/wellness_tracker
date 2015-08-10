d30619a587d81134d531f900a976817e76bd2420
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-10 12:48:53 -0500

    Adds date and number of units to list of permitted params

--- app/controllers/exercise_events_controller.rb
+++ app/controllers/exercise_events_controller.rb
@@ -69,6 +69,6 @@ class ExerciseEventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def exercise_event_params
-      params.require(:exercise_event).permit(:exercise_type_id, :exercise_unit_id, :intensity_id, :date)
+      params.require(:exercise_event).permit(:exercise_type_id, :exercise_unit_id, :intensity_id, :date, :num_units)
     end
 end
