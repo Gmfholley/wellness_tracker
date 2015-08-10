6f45d0d5345575ea7b971f6f5c877aab5be7bfac
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-10 11:03:03 -0500

    Runs migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809221343) do
+ActiveRecord::Schema.define(version: 20150810155936) do
 
   create_table "exercise_events", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "exercise_type_id"
-    t.integer  "duration"
     t.integer  "intensity_id"
-    t.integer  "points"
     t.datetime "date"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.float    "num_units"
+    t.integer  "exercise_unit_id"
   end
 
   create_table "exercise_types", force: :cascade do |t|
