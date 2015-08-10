9e26c15d7d97e18f8adce26c60f132629741d313
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-10 18:45:08 -0500

    Updates routes to remove the unnecessary show friends activities route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'friends_activities/index'
 
-  get 'friends_activities/show'
-
+  get 'friends_activities/index', as: :friends_feed
   resources :friends, except: [:update, :edit, :create]
-  
   post 'friends/new/:user_id' => 'friends#create', as: :add_friend
   
   resources :exercise_types
