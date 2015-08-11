17d6139ce0314250339baccdc86950ca04008632
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-11 18:18:35 -0500

    Adds cheers controller

---
+++ app/controllers/cheers_controller.rb
@@ -0,0 +1,7 @@
+class CheersController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'cheers/create'
+
+  get 'cheers/destroy'
+
   get 'comments/create'
 
   get 'comments/new'
