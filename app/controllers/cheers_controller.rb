3b7ffa528e0506e12945222e0f89cf1c54f480f3
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-11 18:43:17 -0500

    Create method

--- app/controllers/cheers_controller.rb
+++ app/controllers/cheers_controller.rb
@@ -1,17 +1,22 @@
 class CheersController < ApplicationController
-  before_action :set_user
-  before_action :set_friend
+  before_action :set_exercise_event
   
   def create
-    
+    @cheer = Cheer.create(user_id: @user.id, exercise_event_id: @exercise_event.id)
+    if @cheer.errors.empty?
+      render json: @cheer
+    else
+      render json: @cheer.errors
+    end
   end
 
   def destroy
+    
   end
   
   
   private
-  def set_friend
-    @friend = User.find(params["username"])
+  def set_exercise_event
+    @event = ExerciseEvent.find(params["exercise_event_id"])
   end
 end
