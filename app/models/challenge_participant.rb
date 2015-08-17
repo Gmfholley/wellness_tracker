7bb73dd9cda3625cc0147ed7cd10c06ef265b99a
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-17 18:08:17 -0500

    Runs db migrate

--- app/models/challenge_participant.rb
+++ app/models/challenge_participant.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: challenge_participants
+#
+#  id           :integer          not null, primary key
+#  challenge_id :integer
+#  user_id      :integer
+#  team_id      :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class ChallengeParticipant < ActiveRecord::Base
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150817223351) do
+ActiveRecord::Schema.define(version: 20150817230720) do
+
+  create_table "challenge_participants", force: :cascade do |t|
+    t.integer  "challenge_id"
+    t.integer  "user_id"
+    t.integer  "team_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "challenge_types", force: :cascade do |t|
     t.string   "name"
