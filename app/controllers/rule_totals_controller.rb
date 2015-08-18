e2725aa4b754d7fca463ae6a5f11aeaea53b36d7
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-17 22:53:25 -0500

    Adds controller action methods with nothing else

--- app/controllers/rule_totals_controller.rb
+++ app/controllers/rule_totals_controller.rb
@@ -1,2 +1,22 @@
 class RuleTotalsController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def destroy
+  end
+  
+  def update
+  end
+  
+  def new
+  end
 end
