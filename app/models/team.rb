4e6a7af38adcb2761401277da05bf3fe4cb051ee
Gwendolyn Holley <gmfholley@gmail.com>
2015-08-18 15:02:21 -0500

    Adds generate token method and before_crate hook

--- app/models/team.rb
+++ app/models/team.rb
@@ -10,4 +10,16 @@
 #
 
 class Team < ActiveRecord::Base
+  
+  before_create :generate_token
+
+
+  def generate_token
+    self.token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless Challenge.exists?(token: random_token)
+    end
+  end
+  
+  
 end
