08f9cdfaa0e4ae6aeec0fb3e96b90f3ffedc6629
Gwendolyn Holley <gmfholley@gmail.com>
2015-09-26 17:29:31 -0500

    First draft of set month method to get the month from the params for calendar and graph

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -113,8 +113,12 @@ class ActivitiesController < ApplicationController
 
     # Use callbacks to share common setup or constraints between actions.
     def set_month
-      @month = params[:months_past]
-      @date = Chronic.parse("#{@month} months ago")
+      binding.pry
+      begin
+        @month = Date.parse(params["start_date"]).month
+      rescue
+        @month = Date.today.month
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
